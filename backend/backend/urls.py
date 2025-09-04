acd932e16481ac0bcf29204033bf34d5ca514eda
ojaskittur <110086648+ojaskittur@users.noreply.github.com>
2025-09-04 13:00:07 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,5 +14,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-else:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
